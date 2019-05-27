b8f8801d612987e3f539ef31122004dde4562e06
Calin Jula <calinjula@icloud.com>
2019-05-27 21:00:33 +0300

    LoginUtilTest expanded coverage

--- src/test/de/dal33t/powerfolder/util/test/LoginUtilTest.java
+++ src/test/de/dal33t/powerfolder/util/test/LoginUtilTest.java
@@ -19,14 +19,326 @@
  */
 package de.dal33t.powerfolder.util.test;
 
+import de.dal33t.powerfolder.Constants;
+import de.dal33t.powerfolder.Controller;
 import de.dal33t.powerfolder.util.IdGenerator;
 import de.dal33t.powerfolder.util.LoginUtil;
+import de.dal33t.powerfolder.util.Translation;
 import de.dal33t.powerfolder.util.Util;
 import junit.framework.TestCase;
 
 import java.util.Arrays;
 
 public class LoginUtilTest extends TestCase {
+
+    public void testDecorateUrlNullPass() {
+        String url = "http://localhost:8080";
+        String username = "TestUser";
+        char[] password = null;
+
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_USERNAME + "=" + username, LoginUtil.decorateURL(url, username, password));
+
+        String usernameWithSpecial = "Test%23User!";
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_USERNAME + "=" + Util.endcodeForURL(usernameWithSpecial), LoginUtil.decorateURL(url, usernameWithSpecial, password));
+
+    }
+
+    public void testDecorateUrlNullUsername() {
+        String url = "http://localhost:8080";
+        String username = null;
+        char[] password = null;
+
+        assertEquals(url, LoginUtil.decorateURL(url, username, password));
+
+        String usernameEmpty = "";
+        assertEquals(url, LoginUtil.decorateURL(url, username, password));
+    }
+
+
+    public void testDecorateUrlOk() {
+        String url = "http://localhost:8080";
+        String username = "TestUser";
+        char[] password = {'p','a','s','s','A','1','!','w', 'o', 'R', 'd'};
+        System.out.println(LoginUtil.decorateURL(url, username, password));
+
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_USERNAME + "=" + username + "&" + Constants.LOGIN_PARAM_PASSWORD_OBF + "=" + Util.endcodeForURL(LoginUtil.obfuscate(password)),
+                LoginUtil.decorateURL(url, username, password));
+
+    }
+
+    public void testDecorateUrlObfuscatedEmptyUser() {
+        String url = "http://localhost:8080";
+        String username = null;
+        String password = "TestPassA1!";
+
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_PASSWORD_OBF + "=" + Util.endcodeForURL(password), LoginUtil.decorateURL(url, username, password));
+
+        String usernameEmpty = "";
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_PASSWORD_OBF + "=" + Util.endcodeForURL(password), LoginUtil.decorateURL(url, usernameEmpty, password));
+
+        String usernameSpaces = "   ";
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_PASSWORD_OBF + "=" + Util.endcodeForURL(password), LoginUtil.decorateURL(url, usernameSpaces, password));
+
+    }
+
+    public void testDecorateUrlObfuscatedEmptyPassword() {
+        String url = "http://localhost:8080";
+        String username = "TestUsername";
+        String password = null;
+
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_USERNAME + "=" + Util.endcodeForURL(username), LoginUtil.decorateURL(url, username, password));
+
+        String passwordEmpty = "";
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_USERNAME + "=" + Util.endcodeForURL(username), LoginUtil.decorateURL(url, username, passwordEmpty));
+
+        String passwordSpaces = "   ";
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_USERNAME + "=" + Util.endcodeForURL(username), LoginUtil.decorateURL(url, username, passwordSpaces));
+    }
+
+    public void testDecorateUrlObfuscatedOk() {
+        String url = "http://localhost:8080";
+        String username = "TestUsername";
+        String password = "PassA1!Word$%2";
+
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_USERNAME + "=" + Util.endcodeForURL(username) + "&" + Constants.LOGIN_PARAM_PASSWORD_OBF + "=" + Util.endcodeForURL(password)
+                , LoginUtil.decorateURL(url, username, password));
+
+        String usernameSpecialChars = "TestUsernameA1!@#$";
+        String passwordSpaces = "   PassA1!Word$%2   ";
+
+        assertEquals(url + "?" + Constants.LOGIN_PARAM_USERNAME + "=" + Util.endcodeForURL(usernameSpecialChars) + "&" + Constants.LOGIN_PARAM_PASSWORD_OBF + "=" + Util.endcodeForURL(passwordSpaces)
+                , LoginUtil.decorateURL(url, usernameSpecialChars, passwordSpaces));
+    }
+
+    public void testMatchesHashedPwBlank() {
+        assertFalse(LoginUtil.matches(new char[] {'t','s','t'}, ""));
+        assertFalse(LoginUtil.matches(new char[] {'t','s','t'}, null));
+        assertFalse(LoginUtil.matches(new char[] {'t','s','t'}, "   "));
+    }
+
+    public void testHasAndSaltBlankPass() {
+        assertNull(LoginUtil.hashAndSalt(""));
+        assertNull(LoginUtil.hashAndSalt("   "));
+        assertNull(LoginUtil.hashAndSalt(null));
+    }
+
+    public void testClearNullAndEmpty() {
+        char[] password = null;
+        LoginUtil.clear(password);
+        assertNull(password);
+        char[] emptyPassword = {};
+        LoginUtil.clear(emptyPassword);
+        assertTrue(emptyPassword.length == 0);
+    }
+
+    public void testClearOk() {
+
+        char[] password = {'p','a','s','s','A','1','!'};
+        char[] samePassword = {'p','a','s','s','A','1','!'};
+        int passwordLength = password.length;
+        LoginUtil.clear(password);
+        assertTrue(password.length == passwordLength);
+        for (int index = 0; index < passwordLength; index++) {
+            assertTrue(password[index] != samePassword[index]);
+        }
+
+    }
+
+    public void testIsUsernameAny() {
+        Controller controller = new Controller();
+        controller.startDefaultConfig();
+
+        controller.getConfig().put("disableui","true");
+        controller.getConfig().put("server.username.isemail","No");
+
+        assertFalse(LoginUtil.isUsernameAny(controller));
+
+        controller.getConfig().put("server.username.isemail","Yes");
+        assertFalse(LoginUtil.isUsernameAny(controller));
+
+        controller.getConfig().put("server.username.isemail","both");
+        assertTrue(LoginUtil.isUsernameAny(controller));
+
+        controller.getConfig().put("server.username.isemail","BOTH");
+        assertTrue(LoginUtil.isUsernameAny(controller));
+
+        controller.getConfig().put("server.username.isemail","BoTh");
+        assertTrue(LoginUtil.isUsernameAny(controller));
+    }
+
+    public void testIsBooleanConfValueNull() {
+        Controller controller = new Controller();
+        controller.startDefaultConfig();
+
+        controller.getConfig().put("disableui","true");
+        controller.getConfig().setProperty("server.username.isemail", "null");
+
+        assertFalse(LoginUtil.isBoolConfValue(controller));
+
+        controller.getConfig().setProperty("server.username.isemail", "both");
+        assertFalse(LoginUtil.isBoolConfValue(controller));
+
+        controller.getConfig().setProperty("server.username.isemail", "true");
+        assertTrue(LoginUtil.isBoolConfValue(controller));
+        controller.getConfig().setProperty("server.username.isemail", "false");
+        assertTrue(LoginUtil.isBoolConfValue(controller));
+
+        controller.getConfig().setProperty("server.username.isemail", "TRUE");
+        assertTrue(LoginUtil.isBoolConfValue(controller));
+        controller.getConfig().setProperty("server.username.isemail", "FALSE");
+        assertTrue(LoginUtil.isBoolConfValue(controller));
+
+        controller.getConfig().setProperty("server.username.isemail", "TrUe");
+        assertTrue(LoginUtil.isBoolConfValue(controller));
+        controller.getConfig().setProperty("server.username.isemail", "FaLsE");
+        assertTrue(LoginUtil.isBoolConfValue(controller));
+
+        controller.shutdown();
+
+    }
+
+    public void testGetInviteUsernameShibboleth() {
+        Controller controller = new Controller();
+        controller.startDefaultConfig();
+
+        controller.getConfig().put("disableui","true");
+        controller.getConfig().put("server.username.isemail","shibbolethTesting");
+        assertEquals(Translation.get("general.email") + ":", LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.getConfig().put("server.username.isemail","Testbwidm");
+        assertEquals(Translation.get("general.email") + ":", LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.getConfig().put("server.username.isemail","nutzerkennung");
+        assertEquals(Translation.get("general.email") + ":", LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.getConfig().put("server.username.isemail","TTTshiBBolEtH");
+        assertEquals(Translation.get("general.email") + ":", LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.getConfig().put("server.username.isemail","BWIDM");
+        assertEquals(Translation.get("general.email") + ":", LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.getConfig().put("server.username.isemail","nutZerKeNNuNg");
+        assertEquals(Translation.get("general.email") + ":", LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.shutdown();
+    }
+
+    public void testGetInviteUsername() {
+        Controller controller = new Controller();
+        controller.startDefaultConfig();
+        controller.getConfig().put("disableui","true");
+
+        //If it's not Shibboleth then same logic as getUsernameText
+
+        controller.getConfig().put("server.username.isemail","true");
+        assertEquals(Translation.get("general.email") + ":", LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.getConfig().put("server.username.isemail","both");
+        assertEquals(Translation.get("general.username") + '/' + Translation.get("general.email") + ":",
+                LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.getConfig().put("server.username.isemail","false");
+        assertEquals(Translation.get("general.username") + ":", LoginUtil.getInviteUsernameLabel(controller));
+
+        controller.getConfig().put("server.username.isemail","TestingString");
+        assertEquals("TestingString:", LoginUtil.getInviteUsernameLabel(controller));
+
+        //If it is removed, the default value will be considered, which is both
+        controller.getConfig().remove("server.username.isemail");
+        assertEquals(Translation.get("general.username") + '/' + Translation.get("general.email") + ":",
+                LoginUtil.getInviteUsernameLabel(controller));
+
+
+
+    }
+
+    public void testGetUsernameTextNull() {
+        Controller controller = null;
+
+        try {
+            LoginUtil.getUsernameText(controller);
+            fail("Did not reject controller when controller was null");
+        } catch (NullPointerException e){
+            //OK since controller was null
+        }
+    }
+
+    public void testGetUsernameTestOk() {
+        Controller controller = new Controller();
+        controller.startDefaultConfig();
+        controller.getConfig().put("disableui", "true");
+
+        controller.getConfig().put("server.username.isemail","true");
+        assertEquals(Translation.get("general.email"), LoginUtil.getUsernameText(controller));
+
+        controller.getConfig().put("server.username.isemail","both");
+        assertEquals(Translation.get("general.username") + '/' + Translation.get("general.email"),
+                LoginUtil.getUsernameText(controller));
+
+        controller.getConfig().put("server.username.isemail","false");
+        assertEquals(Translation.get("general.username"), LoginUtil.getUsernameText(controller));
+
+        controller.getConfig().put("server.username.isemail","ThisIsATest");
+        assertEquals("ThisIsATest", LoginUtil.getUsernameText(controller));
+
+        //If it is removed, the default value will be considered, which is both
+        controller.getConfig().remove("server.username.isemail");
+        assertEquals(Translation.get("general.username") + '/' + Translation.get("general.email"),
+                LoginUtil.getUsernameText(controller));
+    }
+
+    public void testIsUsernameEmailOnlyTest() {
+        Controller controller = new Controller();
+        controller.startDefaultConfig();
+        controller.getConfig().put("disableui", "true");
+
+        controller.getConfig().put("server.username.isemail","both");
+        assertFalse(LoginUtil.isUsernameEmailOnly(controller));
+
+        controller.getConfig().put("server.username.isemail","Testing");
+        assertFalse(LoginUtil.isUsernameEmailOnly(controller));
+
+        controller.getConfig().put("server.username.isemail","true");
+        assertTrue(LoginUtil.isUsernameEmailOnly(controller));
+
+        controller.getConfig().put("server.username.isemail","false");
+        assertFalse(LoginUtil.isUsernameEmailOnly(controller));
+    }
+
+    public void testIsValidUsername() {
+        Controller controller = new Controller();
+        controller.startDefaultConfig();
+        controller.getConfig().put("disableui", "true");
+
+        assertFalse(LoginUtil.isValidUsername(controller,""));
+        assertFalse(LoginUtil.isValidUsername(controller,"    "));
+
+        controller.getConfig().put("server.username.isemail","both");
+        assertTrue(LoginUtil.isValidUsername(controller,"TestUser"));
+
+        controller.getConfig().put("server.username.isemail","true");
+        assertTrue(LoginUtil.isValidUsername(controller, "test@testing.com"));
+        assertFalse(LoginUtil.isValidUsername(controller, "TestEmailWrong"));
+        assertFalse(LoginUtil.isValidUsername(controller, ""));
+
+        controller.getConfig().put("server.username.isemail","false");
+        assertTrue(LoginUtil.isValidUsername(controller, "UserOne"));
+        assertTrue(LoginUtil.isValidUsername(controller,"Test@testing.com"));
+        assertFalse(LoginUtil.isValidUsername(controller, "   "));
+    }
+
+    public void testSatisfiesUnixPoicyNull() {
+        assertFalse(LoginUtil.satisfiesUnixPolicy(null));
+        String pass = null;
+        assertFalse(LoginUtil.satisfiesUnixPolicy(pass));
+    }
+
+    public void testDeobfuscateIllegalArgument() {
+        assertNull(LoginUtil.deobfuscate("!"));
+        assertNull(LoginUtil.deobfuscate("\""));
+    }
+
+
     public void testObfuscate() {
         String password = "xC33öcn$k3444o$$44";
         String obf = LoginUtil.obfuscate(password.toCharArray());
